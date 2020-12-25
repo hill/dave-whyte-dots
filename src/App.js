59809705cee6032887100a22003685c53d2aaf20
Tom Hill <tom@hill.xyz>
2020-12-25 20:58:30 +1100

    Basic demo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,74 @@
-import React, { useRef } from 'react'
+// https://tympanus.net/codrops/2020/12/17/recreating-a-dave-whyte-animation-in-react-three-fiber/
+import React, { useLayoutEffect, useMemo, useRef } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
-import { OrbitControls } from '@react-three/drei'
+import * as THREE from 'three'
 
-export default function App() {
+const roundedSquareWave = (t, delta, a, f) => {
+  return ((2 * a) / Math.PI) * Math.atan(Math.sin(2 * Math.PI * t * f) / delta)
+}
+
+function Dots() {
+  const ref = useRef()
+
+  const { vec, transform, positions, distances } = useMemo(() => {
+    const vec = new THREE.Vector3()
+    const transform = new THREE.Matrix4()
+
+    const positions = [...Array(10000)].map((_, i) => {
+      const position = new THREE.Vector3()
+      position.x = (i % 100) - 50
+      position.y = Math.floor(i / 100) - 50
+      position.y += (i % 2) * 0.5 // (hex pattern)
+      position.x += Math.random() * 0.3
+      position.y += Math.random() * 0.3
+      return position
+    })
+
+    const right = new THREE.Vector3(1, 0, 0)
+    const distances = positions.map(
+      (pos) => pos.length() + Math.cos(pos.angleTo(right) * 8) * 0.5 // hexagon wave out
+    )
+
+    return { vec, transform, positions, distances }
+  }, [])
+
+  useFrame(({ clock }) => {
+    // const scale = 1 + Math.sin(clock.elapsedTime) * 0.3
+    const speed = 1 / 3.8
+    const zoomDistance = 0.4
+    for (let i = 0; i < 10000; i++) {
+      const dist = distances[i]
+      // distance affects the wave phase
+      const t = clock.elapsedTime - dist / 25
+      const cornerCurveSharpness = 0.15 + (0.2 * dist) / 72 // bad name.
+      const wave = roundedSquareWave(t, cornerCurveSharpness, zoomDistance, speed)
+      const scale = 1.3 + wave
+
+      // scale iniital position by our oscillator
+      vec.copy(positions[i]).multiplyScalar(scale)
+
+      // apply vector3 to a matrix4
+      transform.setPosition(vec)
+
+      // update matrix4 for this instance
+      ref.current.setMatrixAt(i, transform)
+    }
+    ref.current.instanceMatrix.needsUpdate = true
+  })
+
   return (
-    <Canvas>
-      <color attach="background" args={['black']} />
-      <OrbitControls />
-      <Thing />
-    </Canvas>
+    <instancedMesh ref={ref} args={[null, null, 10000]}>
+      <circleBufferGeometry args={[0.15]} />
+      <meshBasicMaterial />
+    </instancedMesh>
   )
 }
 
-function Thing() {
-  const ref = useRef()
-  useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y += 0.01
-  })
+export default function App() {
   return (
-    <mesh
-      ref={ref}
-      onClick={(e) => console.log('click')}
-      onPointerOver={(e) => console.log('hover')}
-      onPointerOut={(e) => console.log('unhover')}
-    >
-      <boxBufferGeometry />
-      <meshNormalMaterial />
-    </mesh>
+    <Canvas orthographic camera={{ zoom: 20 }} colorManagement={false}>
+      <color attach="background" args={['black']} />
+      <Dots />
+    </Canvas>
   )
 }
---
+++ src/Effects.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three'
+import { useThree, useFrame, extend } from 'react-three-fiber'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { SavePass } from 'three/examples/jsm/postprocessing/SavePass'
+import { CopyShader } from 'three/examples/jsm/shaders/CopyShader'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { useEffect } from 'react'
+
+extend({ EffectComposer, ShaderPass, SavePass, RenderPass })
+
+const triColorMix = {
+  uniforms: {
+    tDiffuse1: { value: null },
+    tDiffuse2: { value: null },
+    tDiffuse3: { value: null },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1);
+    }
+  `,
+  fragmentShader: `
+    varying vec2 vUv;
+    uniform sampler2D tDiffuse1;
+    uniform sampler2D tDiffuse2;
+    uniform sampler2D tDiffuse3;
+    
+    void main() {
+      vec4 del0 = texture2D(tDiffuse1, vUv);
+      vec4 del1 = texture2D(tDiffuse2, vUv);
+      vec4 del2 = texture2D(tDiffuse3, vUv);
+      float alpha = min(min(del0.a, del1.a), del2.a);
+      gl_FragColor = vec4(del0.r, del1.g, del2.b, alpha);
+    }
+  `,
+}
+
+function Effects() {
+  const composer = useRef()
+  const { scene, gl, size, camera } = useThree()
+
+  const savePass = useRef()
+  const blendPass = useRef()
+  const swap = useRef(false) // Whether to swap the delay buffers
+  const { rtA, rtB } = useMemo(() => {
+    const rtA = new THREE.WebGLRenderTarget(size.width, size.height)
+    const rtB = new THREE.WebGLRenderTarget(size.width, size.height)
+    return { rtA, rtB }
+  }, [size])
+
+  useEffect(() => void composer.current.setSize(size.width, size.height), [size])
+  useFrame(() => {
+    composer.current.render()
+  }, 1)
+
+  return (
+    <effectComposer ref={composer} args={[gl]}>
+      <renderPass attachArray="passes" scene={scene} camera={camera} />
+      <shaderPass attachArray="passes" ref={blendPass} args={[triColorMix, 'tDiffuse1']} needsSwap={false} />
+      <savePass attachArray="passes" ref={savePass} needsSwap={true} />
+      <shaderPass attachArray="passes" args={[CopyShader]} />
+    </effectComposer>
+  )
+}
